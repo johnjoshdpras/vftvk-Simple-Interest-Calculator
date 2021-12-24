51dbdf05233fa9e507a8e4d0a2aa4dd6c6d01da2
Joshua D. Prasetyo <artjo26@gmail.com>
2021-12-24 23:01:16 +0800

    changes made from my laptop

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    
+    //find the mathematical value of interest
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount = parseFloat(interest);
     //to validate principal number
     if(principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-    //find the mathematical value of interest
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var amount = parseFloat(interest);
-    
     //Show the result, toFixed(2) to show only 2 decimals
     document.getElementById("result").innerHTML="If you deposit <mark>"+(principal)+"</mark>,\<br\>at an interest rate of <mark>"+(rate)+"</mark>%\<br\>You will receive an amount of <mark>"+amount.toFixed(2)+"</mark>,\<br\>in the year <mark>"+(year)+"</mark>\<br\>"
 }
